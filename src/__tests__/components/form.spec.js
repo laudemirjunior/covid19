76ceef22658b07afcdc1252b417c3d7e4b9c11e4
Laudemir <nlaudemir@gmail.com>
2022-03-25 21:26:22 -0300

    feat: create readme.md

--- src/__tests__/components/form.spec.js
+++ src/__tests__/components/form.spec.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  getByText,
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Form } from "../../components/form";
 
 describe("Form component", () => {
